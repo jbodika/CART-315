e63c1c76be326d0ac9509d86b0ad64df79d493a0
Jolene <joleneodia@hotmail.com>
2026-03-12 02:12:52 -0400

    added the click to socket functionality
    player can click on the monitor and it will changes "screens" to show the connecting area of the monitor

--- Projects/GamePrototype/Assets/Scripts/Monitor.cs
+++ Projects/GamePrototype/Assets/Scripts/Monitor.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Monitor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject image1;
+    public GameObject image2;
+    public GameObject image3;
+    public GameObject image4;
+
     void Start()
     {
-        
+        image1.SetActive(false);
+        image2.SetActive(false);
+        image3.SetActive(false);
+        image4.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        Debug.Log("Monitor Clicked");
+
+        // destroy monitor
+        Destroy(gameObject);
+
+        // show the new images
+        image1.SetActive(true);
+        image2.SetActive(true);
+        image3.SetActive(true);
+        image4.SetActive(true);
     }
+
 
     // click on monitor function 
+
 
     // click on cable box
 
